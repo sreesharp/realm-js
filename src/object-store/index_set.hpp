e88fe14a9d4a61f71eb744844d6a9c8b45365f54
Sebastian Lauwers <sl@realm.io>
2015-09-17 12:12:24 +0200

    Fixed unknown `size_t`

--- src/object-store/index_set.hpp
+++ src/object-store/index_set.hpp
@@ -19,43 +19,44 @@
 #ifndef REALM_INDEX_SET_HPP
 #define REALM_INDEX_SET_HPP
 
+#include <cstdlib>
 #include <vector>
 
 namespace realm {
 class IndexSet {
 public:
-    using value_type = std::pair<size_t, size_t>;
+    using value_type = std::pair<std::size_t, std::size_t>;
     using iterator = std::vector<value_type>::iterator;
     using const_iterator = std::vector<value_type>::const_iterator;
 
     const_iterator begin() const { return m_ranges.begin(); }
     const_iterator end() const { return m_ranges.end(); }
     bool empty() const { return m_ranges.empty(); }
-    size_t size() const { return m_ranges.size(); }
+    std::size_t size() const { return m_ranges.size(); }
 
     // Add an index to the set, doing nothing if it's already present
-    void add(size_t index);
+    void add(std::size_t index);
 
     // Remove all indexes from the set and then add a single range starting from
     // zero with the given length
-    void set(size_t len);
+    void set(std::size_t len);
 
     // Insert an index at the given position, shifting existing indexes at or
     // after that point back by one
-    void insert_at(size_t index);
+    void insert_at(std::size_t index);
 
     // Add an index which has had all of the ranges in the set before it removed
-    void add_shifted(size_t index);
+    void add_shifted(std::size_t index);
 
 private:
     std::vector<value_type> m_ranges;
 
     // Find the range which contains the index, or the first one after it if
     // none do
-    iterator find(size_t index);
+    iterator find(std::size_t index);
     // Insert the index before the given position, combining existing ranges as
     // applicable
-    void do_add(iterator pos, size_t index);
+    void do_add(iterator pos, std::size_t index);
 };
 } // namespace realm
 
