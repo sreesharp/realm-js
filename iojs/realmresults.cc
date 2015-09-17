f5ac6227fd61be2219f57b970ac44cd289b6b0fa
Ari Lazier <alazier@gmail.com>
2015-09-17 16:56:27 +0200

    partially hook up results

--- iojs/realmresults.cc
+++ iojs/realmresults.cc
@@ -1,12 +1,15 @@
 #include <node.h>
 
 #include "realmresults.h"
+#include "realmutils.hpp"
 
 using namespace v8;
+using namespace realm;
 
 Persistent<Function> RealmResults::constructor;
 
-RealmResults::RealmResults() {}
+RealmResults::RealmResults(realm::Results results) : m_results(std::move(results)) {
+}
 
 RealmResults::~RealmResults() {}
 
@@ -24,6 +27,7 @@ void RealmResults::Init(Handle<Object> exports) {
     exports->Set(String::NewFromUtf8(isolate, "RealmResults"), tpl->GetFunction());
 }
 
+
 void RealmResults::New(const FunctionCallbackInfo<Value>& args) {
     Isolate* isolate = Isolate::GetCurrent();
     HandleScope scope(isolate);
@@ -34,10 +38,40 @@ void RealmResults::New(const FunctionCallbackInfo<Value>& args) {
     } else {
         // TODO: Invoked as plain function `RealmResults(...)`, turn into construct call.
     }
+
+}
+bool ValidateArgumentRange(size_t count, size_t min, size_t max) {
+    if (count < min || count > max)
+    {
+        makeError(Isolate::GetCurrent(), "argument count must be between " + std::to_string(min) + " and " + std::to_string(max));
+        return false;
+    }
+    return true;
 }
 
 void RealmResults::SortByProperty(const v8::FunctionCallbackInfo<v8::Value>& args) {
+    RealmResults *results = ObjectWrap::Unwrap<RealmResults>(args.This());
+
     Isolate* isolate = Isolate::GetCurrent();
     HandleScope scope(isolate);
 
+    if (!ValidateArgumentRange(args.Length(), 1, 2)) {
+        return;
+    }
+
+    std::string propName = ToString(args[0]);
+    Property *prop = results->m_results.object_schema.property_for_name(propName);
+    if (!prop) {
+        makeError(isolate, "Property '" + propName + "' does not exist on object type '" + 
+            results->m_results.object_schema.name + "'");
+        return;
+    }
+
+    bool ascending = true;
+    if (args.Length() == 2) {
+        ascending = *args[1]->ToBoolean();
+    }
+
+    SortOrder sort = {{prop->table_column}, {ascending}};
+    results->m_results.setSort(sort);
 }
