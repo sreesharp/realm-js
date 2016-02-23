981838c9a5713d1ef81a1d7cf58e791e844c9d41
Ari Lazier <alazier@gmail.com>
2016-02-23 12:16:33 -0800

    android runs

--- examples/AsyncExample/index.android.js
+++ examples/AsyncExample/index.android.js
@@ -11,19 +11,51 @@ import React, {
   View
 } from 'react-native';
 
+
+var Realm = require('realm');
+
+var TestObjectSchema = {
+  name: 'TestObject',
+  properties: { string : 'string' }
+}
+
 class AsyncExample extends Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      text: "",
+    };
+  }
+
+  async runAsyncQuery() {
+    var count = 10000;
+    this.setState({text: "Creating " + count + " objects."});
+    var realm = new Realm({schema: [TestObjectSchema]});
+    realm.write(() => {
+      for (var i = 0; i < count; i++) {
+        realm.create('TestObject', {string: "" + i});
+      }
+    });
+
+    this.setState({text: "Querying."});
+
+    realm.objects('TestObject').filteredSnapshot("string endswith '1'", (results) => {
+        console.log("Got query");
+        this.setState({text: "Recieved " + results.length + " query results."});
+    });
+  }
+  
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
+          <Text style={styles.button} onPress={this.runAsyncQuery.bind(this)}>
+              Tap to Run Async Test
+          </Text>
+          <Text>{this.state.text}</Text>
+          <Text style={styles.instructions}>
+              Press Cmd+R to reload,{'\n'}
+              Cmd+D or shake for dev menu
+          </Text>
       </View>
     );
   }
